100b4a72423d4bdf58220eb98a905aa4fee9edb2
qsaleh <qmsaleh@gmail.com>
2020-01-16 23:40:44 +0000

    created breedfetcher and test file

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,14 +1,19 @@
 const request = require('request');
-//const fs = require('fs');
 const inputURL = 'https://api.thecatapi.com/v1/breeds/search?';
-const inputBreed = process.argv[2];
 
-request(`${inputURL}q=${inputBreed}`, (error, result, body) => {
-  const data = JSON.parse(body);
-  if (data[0] === undefined) {
-    console.log('Breed undefined.');
-    return;
-  } else {
-    console.log(data[0].description);
-  }
-});
+const fetchBreedDescription = function(breedName, callback) {
+  request(`${inputURL}q=${breedName}`, (error, result, body) => {
+    const data = JSON.parse(body);
+    if (error !== null) {
+      callback(error, null);
+    } else if (data.length === 0) {
+      console.log('Error, undefined breed.');
+    } else {
+      callback(null, data[0].description);
+    }
+  });
+};
+
+
+
+module.exports = { fetchBreedDescription };
---
+++ index.js
@@ -0,0 +1,11 @@
+const { fetchBreedDescription } = require('./breedFetcher');
+
+const breedName = process.argv[2];
+
+fetchBreedDescription(breedName, (error, desc) => {
+  if (error) {
+    console.log('Error, undefined breed.');
+  } else {
+    console.log(desc);
+  }
+});
